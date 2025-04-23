a1e1e9612b30a4c3f7cca21b92b7dd818660824c
Anand Manuvanjay Awasthi <166633429+Anand-Manuvanjay-Awasthi@users.noreply.github.com>
2025-04-23 18:54:00 +0530

    Update Client.java

--- Multithreaded proxy web server/src/Client.java
+++ Multithreaded proxy web server/src/Client.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int port = 8080;
         String host = "localhost";
         InetAddress address = InetAddress.getByName(host);
